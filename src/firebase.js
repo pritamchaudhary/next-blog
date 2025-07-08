d33864a91eac523e325cb4ef92fa8c94713ff5e3
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2025-07-09 00:04:52 +0530

    add firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "next-blog-bac5b.firebaseapp.com",
-  projectId: "next-blog-bac5b",
-  storageBucket: "next-blog-bac5b.firebasestorage.app",
-  messagingSenderId: "282459795765",
-  appId: "1:282459795765:web:9392a74d8014b012c2dc7c"
+  authDomain: "next-blog-a0857.firebaseapp.com",
+  projectId: "next-blog-a0857",
+  storageBucket: "next-blog-a0857.firebasestorage.app",
+  messagingSenderId: "748391738383",
+  appId: "1:748391738383:web:a697002e4bae0ff19c477a"
 };
 
 // Initialize Firebase
